b4f05b28adf1ff1d23a16e5ee3ce62754dca346a
sachinfrayne <sachin.frayne@gmail.com>
2026-02-05 13:36:42 +0100

    adds the files to stand up the clusters

---
+++ es-9.3-vs-os-3.4-vector-search/opensearch-gcp/os.tf
@@ -0,0 +1,30 @@
+resource "google_container_cluster" "os_context_engineering_benchmark" {
+  name                = "os-context-engineering-benchmark"
+  location            = "us-central1-a"
+  deletion_protection = false
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "os_context_engineering_nodes" {
+  name       = "os-context-engineering-nodepool"
+  cluster    = google_container_cluster.os_context_engineering_benchmark.id
+  node_count = 3
+
+  node_config {
+    machine_type = "e2-standard-2"
+    disk_size_gb = 50
+  }
+}
+
+resource "google_container_node_pool" "osd_context_engineering_node" {
+  name       = "osd-context-engineering-nodepool"
+  cluster    = google_container_cluster.os_context_engineering_benchmark.id
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 120
+  }
+}
